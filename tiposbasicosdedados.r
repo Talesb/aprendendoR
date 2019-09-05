a7a453ef355df4c6a1c3b954f0be8d6bb2fdd62b
Talesb <talesbatistafig@hotmail.com>
2019-09-05 17:41:47 -0300

    Tipos básicos de dados

---
+++ tiposbasicosdedados.r
@@ -0,0 +1,33 @@
+# Tipos básicos de dados
+# Números 
+
+# Inteiro - Integer 
+4L
+numero <- 4L
+
+#integer
+class(numero)
+
+#ponto flutuante  - Numeric
+4
+numero <- 4
+
+#numeric
+class(numero)
+
+#Caractere - character
+"Quatro"
+#ou
+'Quatro'
+numero <- "Quatro"
+
+#character
+class("Quatro")
+
+#Lógico Booleano -TRUE ou FALSE/ T ou F
+TRUE 
+numero <- TRUE
+
+#Logical
+class(numero)
+
