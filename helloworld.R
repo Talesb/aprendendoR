0d89f2e8ff3dd7a14d1dd4d50aa8d2615c25570d
Talesb <talesbatistafig@hotmail.com>
2019-09-05 16:37:26 -0300

    first commit

---
+++ helloworld.R
@@ -0,0 +1,24 @@
+#Operacoes basicas
+print('Adicao')
+4+5
+56+76
+986+3
+print('Divisao')
+76/9
+4/2
+69/3
+print('Multiplicacao')
+443*2
+44*2
+23*3
+print('Diferenca')
+59-45
+23-4
+98-143
+print('Exponenciacao')
+3^9
+print('Resto de divisao')
+5 %% 2
+print('Ordem de parenteses')
+2 *(2+1) - 3
+
