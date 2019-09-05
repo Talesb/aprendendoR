d25eb9ff72947a0a059cbc383ecf27c40162a23e
Talesb <talesbatistafig@hotmail.com>
2019-09-05 17:19:22 -0300

    Criando primeiras variaveis, listando e limpando workspace

--- criandovariaveis.r
+++ criandovariaveis.r
@@ -9,3 +9,17 @@ nome <- 'Tales B'
 # Tales b
 nome
  
+ #Lista as variáveis já declaradas
+ ls()
+
+#Remove as variáveis já declaradas
+rm(variavel)
+
+# nome
+ls()
+
+#Remove todas as variáveis já declaradas
+rm(list = ls()) 
+
+#character(0)
+ls()
