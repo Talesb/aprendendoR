a238eb4346dab12c267a363fdbb3b8516da9f3db
Talesb <talesbatistafig@hotmail.com>
2019-09-16 23:50:59 -0300

    Trabalhando com operacoes matematicas em vetores

---
+++ operacoesmatematicaemvetores.r
@@ -0,0 +1,42 @@
+#Vetor a ser Calculado
+x <- c(1:10)
+
+#Soma
+x+10
+
+#Multiplicacao
+x*10
+
+xMultiplicado <- x * 10
+xMultiplicado 
+
+
+#Multiplicacao entre vetores
+mult <- c(2:12)
+
+mult
+
+#x*mult = Erro - O comprimento tem que ser igual ou multiplo
+mult <- c(2:11)
+x
+mult
+x*mult
+
+#Vetor menor que o alvo e não e multiplo
+y <- c(1:3)
+y
+
+# x*y = erro
+x*y
+
+#Ajustar
+y<- c(2,3)
+
+x
+y
+x*y
+
+#Multiplica e vai repetindo -> " 2  6  6 12 10 18 14 24 18 30"
+ 
+
+
